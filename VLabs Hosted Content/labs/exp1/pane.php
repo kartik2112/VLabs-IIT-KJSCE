124fd9a8b7720458007ae89deb794abe0f7f2390
Varun Rao <varun21exps@gmail.com>
2017-04-04 10:49:27 +0530

    Pane Bug fixed.

--- VLabs Hosted Content/labs/exp1/pane.php
+++ VLabs Hosted Content/labs/exp1/pane.php
@@ -1,4 +1,6 @@
-   
+<?php
+    session_start();
+?>   
 
       <!-- Left side column. contains the logo and sidebar -->
       <aside class="main-sidebar">
@@ -11,31 +13,56 @@
           <!-- /.sear ch form -->
           <!-- sidebar menu: : style can be found in sidebar.less -->
             <ul class="sidebar-menu">
-                <li class="active treeview">
+                <?php
+                    if($_SESSION["currPage"]==1)
+                        echo '<li class="active treeview">';
+                    else
+                        echo '<li class="treeview">';
+                ?>
                     <a href="index.php">
                         <i class="fa fa-files-o"></i> <span>Aim</span>
 
                     </a>
                 </li>
-                <li class="treeview">
+                <?php
+                    if($_SESSION["currPage"]==2)
+                        echo '<li class="active treeview">';
+                    else
+                        echo '<li class="treeview">';
+                ?>
                     <a href="theory.php">
                         <i class="fa fa-files-o"></i>
                         <span>Theory</span>
                     </a>
                 </li>
-                <li class="treeview">
+                <?php
+                    if($_SESSION["currPage"]==3)
+                        echo '<li class="active treeview">';
+                    else
+                        echo '<li class="treeview">';
+                ?>
                     <a href="pretest.php">
                         <i class="fa fa-files-o"></i> <span>Pre Test</span>
 
                     </a>
                 </li>
                 
-                <li class="treeview">
+                <?php
+                    if($_SESSION["currPage"]==4)
+                        echo '<li class="active treeview">';
+                    else
+                        echo '<li class="treeview">';
+                ?>
                     <a href="procedure.php">
                         <i class="fa fa-files-o"></i> <span>Procedure</span>
                     </a>
                 </li>
-                <li class="treeview">
+                <?php
+                    if($_SESSION["currPage"]==5)
+                        echo '<li class="active treeview">';
+                    else
+                        echo '<li class="treeview">';
+                ?>
                     <a href="simulation.php">
                         <i class="fa fa-laptop"></i>
                         <span>Simulation</span>
@@ -44,7 +71,12 @@
                     </a>
                 </li>
 
-                <li class="treeview">
+                <?php
+                    if($_SESSION["currPage"]==6)
+                        echo '<li class="active treeview">';
+                    else
+                        echo '<li class="treeview">';
+                ?>
                     <a href="result.php">
                         <i class="fa fa-laptop"></i>
                         <span>Result</span>
@@ -53,7 +85,12 @@
                     </a>
                 </li>
                 
-                <li class="treeview">
+                <?php
+                    if($_SESSION["currPage"]==7)
+                        echo '<li class="active treeview">';
+                    else
+                        echo '<li class="treeview">';
+                ?>
                     <a href="posttest.php">
                         <i class="fa fa-files-o"></i>
                         <span>Post Test</span>
@@ -61,7 +98,12 @@
                         </span>
                     </a>
                 </li>
-                <li class="treeview">
+                <?php
+                    if($_SESSION["currPage"]==8)
+                        echo '<li class="active treeview">';
+                    else
+                        echo '<li class="treeview">';
+                ?>
                     <a href="references.php">
                         <i class="fa fa-files-o"></i>
                         <span>References</span>
--- VLabs Hosted Content/labs/exp1/procedure.php
+++ VLabs Hosted Content/labs/exp1/procedure.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+    $_SESSION["currPage"] = 4;
+?>
 <html>
     <head>
         <meta charset="utf-8">
@@ -54,7 +58,7 @@
                     </section>
                 </nav>
             </header>
-            <?php include 'pane.html'; ?>
+            <?php include 'pane.php'; ?>
             <!-- Content Wrapper. Contains page content -->
             <div class="content-wrapper">
                 <!-- Content Header (Page header) -->
