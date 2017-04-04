861c61122c442ea5b10c7ac2b76f9fdcab04b9dc
dev-td7 <tejas.dastane@gmail.com>
2017-04-04 12:09:17 +0530

    Weights changing added

--- VLabs Hosted Content/labs/exp2/simulation.php
+++ VLabs Hosted Content/labs/exp2/simulation.php
@@ -29,10 +29,6 @@
         <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
         <script type="text/javascript">
-          
-          $(document).ready(){
-              $(".neuron").tooltip();
-          }
 
           $(function(){
             $(".sliders").slider({
