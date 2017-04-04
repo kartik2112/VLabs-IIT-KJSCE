610a6ccac5321536742c4e0bddc8a4176e710d55
Kartik Shenoy <kartik.shenoyy@gmail.com>
2017-04-04 16:16:12 +0530

    JS moved, next itrn button added, colors changed, disabled attr added to all disabled buttons

--- VLabs Hosted Content/labs/exp1/simulation.php
+++ VLabs Hosted Content/labs/exp1/simulation.php
@@ -29,229 +29,15 @@
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
         <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
-        <script type="text/javascript">
-            var AND_threshold = 0;
-            var AND_w1 = 1;
-            var AND_w2 = 1;
-            var AND_calcOP;
-            var timer1, timer2, timer3,timer4;
-
-            $(document).ready(function () {
-                /*
-                Initialize sliders and add listeners
-                */
-                $("#AND_Gate_Threshold_slider").slider({
-                    max: 6.5,
-                    min: -0.5,
-                    step: 0.1,
-                    slide: function (event, ui) {
-                        $("#AND-threshold-value").text("Threshold: " + ui.value);
-                        AND_threshold = ui.value;
-                    }
-                });
-                $("#AND-threshold-value").text("Threshold: " + $("#AND_Gate_Threshold_slider").slider("value"));
-                AND_threshold = $("#AND_Gate_Threshold_slider").slider("value");
-
-
-
-                $("#AND_Gate_w1_slider").slider({
-                    max: 3,
-                    min: 0,
-                    value: 1,
-                    step: 0.05,
-                    slide: function (event, ui) {
-                        $(".AND-inputX-oplay_neuron1-weight").text(ui.value);
-                        AND_w1 = ui.value;
-                    }
-                });
-
-                $(".AND-inputX-oplay_neuron1-weight").text($("#AND_Gate_w1_slider").slider("value"));
-                AND_w1 = $("#AND_Gate_w1_slider").slider("value");
-
-
-                $("#AND_Gate_w2_slider").slider({
-                    max: 3,
-                    min: 0,
-                    value: 1,
-                    step: 0.05,
-                    slide: function (event, ui) {
-                        $(".AND-inputY-oplay_neuron1-weight").text(ui.value);
-                        AND_w2 = ui.value;
-                    }
-                });
-
-                $(".AND-inputY-oplay_neuron1-weight").text($("#AND_Gate_w2_slider").slider("value"));
-                AND_w2 = $("#AND_Gate_w2_slider").slider("value");
-
-            });
-
-
-            function displayDiv(val) {
-                if (val == "AND") {
-                    $("#OR-gate-sim").slideUp(400);
-                    $("#NOT-gate-sim").slideUp(400);
-                    $("#AND-gate-sim").slideDown(400);
-                }
-                else if (val == "OR") {
-                    $("#NOT-gate-sim").slideUp(400);
-                    $("#AND-gate-sim").slideUp(400);
-                    $("#OR-gate-sim").slideDown(400);
-                }
-                else {
-                    $("#OR-gate-sim").slideUp(400);
-                    $("#AND-gate-sim").slideUp(400);
-                    $("#NOT-gate-sim").slideDown(400);
-                }
-                stopSimulation();
-            }
-
-            function stopSimulation(timer) {
-                resetCompleteSimulation();
-                $(".sliders").slider("enable");
-                $("#startSimButton").removeClass("disabled");
-                $("#selGate").prop("disabled", false);
-                $("#stopSimButton").addClass("disabled");
-                window.clearInterval(timer);
-                window.clearTimeout(timer1);
-                window.clearTimeout(timer2);
-                window.clearTimeout(timer3);
-                window.clearTimeout(timer4);
-            }
-
-            function resetCompleteSimulation() {
-                resetSimulationPart();
-                $(".AND-TT-rows").removeClass("highlightTTRow");
-                $(".AND-TT-OP-rows").text("");
-            }
-
-            function resetSimulationPart() {
-                $(".changingTextStyle").text("");
-
-                $(".StdLine").removeClass("animatedLineGreen");
-                $(".StdLine").removeClass("animatedLinePurple");
-                $(".StdLine").removeClass("animatedLine");
-            }
-
-            function simulateANDGate(iterationNo, inputX, inputY, w1, w2, threshold, interval, timer) {
-                /* This is the way arrays were getting passed as String */
-                inputX = inputX.split(",");
-                inputY = inputY.split(",");
-
-                resetSimulationPart();
-
-                /*
-                Remove all highlights from truth table and highlight current truth table row
-                */
-                $(".AND-TT-rows").removeClass("highlightTTRow");
-                $("#AND-TT-row-" + (iterationNo + 1)).addClass("highlightTTRow");
-
-                /*
-                Set animations on lines
-                */
-                $(".AND-XVal").text(inputX[iterationNo]);
-                $(".AND-YVal").text(inputY[iterationNo]);
-                $("#AND-inputX-oplay_neuron1").addClass(inputX[iterationNo] == 1 ? "animatedLineGreen" : "animatedLinePurple");
-                $("#AND-inputY-oplay_neuron1").addClass(inputY[iterationNo] == 1 ? "animatedLineGreen" : "animatedLinePurple");
-                var ux = w1 * inputX[iterationNo] + w2 * inputY[iterationNo]; /* Calculate intermediate, u(x) */
-                //alert(w1 + " " + w2 + " " + inputX[iterationNo] + " " + inputY[iterationNo] + " " + ux);
-
-                timer1 = window.setTimeout(function () {
-                    /* Display u(x) and pause */
-                    $("#AND-ux-value").text("= " + ux);
-
-                    timer2 = window.setTimeout(function () {
-                        /* Display threshold calculation and pause */
-                        $("#AND-oplay_neuron1-oplay_thrshld").addClass("animatedLine");
-                        if (ux >= threshold) {
-                            $("#AND-yx-value-expln").text("u(x) = " + ux + " >= " + threshold);
-                        }
-                        else {
-                            $("#AND-yx-value-expln").text("u(x) = " + ux + " < " + threshold);
-                        }
-
-                        timer3 = window.setTimeout(function () {
-                            /* Display calculation output as y(x) and add to truth table */
-                            if (ux >= threshold) {
-                                $("#AND-yx-value-expln").text("u(x) = " + ux + " >= " + threshold + " ⇒ y(x) = 1"); /* Change explanation to include y(x) */
-                                $("#AND-TT-OP-row-" + (iterationNo + 1)).text(1); /* Add entry in truth table */
-                                $("#AND-yx-value").text("= " + 1);
-                                AND_calcOP.push(1);
-                            }
-                            else {
-                                $("#AND-yx-value-expln").text("u(x) = " + ux + " < " + threshold + " ⇒ y(x) = 0"); /* Change explanation to include y(x) */
-                                $("#AND-TT-OP-row-" + (iterationNo + 1)).text(0); /* Add entry in truth table */
-                                $("#AND-yx-value").text("= " + 0);
-                                AND_calcOP.push(0);
-                            }
-
-                        }, interval * 6);
-                    }, interval * 4);
-
-                }, interval * 2);
-            }
-
-            function startSimulation(interval) {
-                resetCompleteSimulation();
-
-                var inputXofAND = [0, 0, 1, 1];
-                var inputYofAND = [0, 1, 0, 1];
-                var iterationNo = 0;
-                AND_calcOP = new Array();
-
-                $(".sliders").slider("disable");
-                $("#startSimButton").addClass("disabled");
-                $("#selGate").prop("disabled", true);
-                $("#stopSimButton").removeClass("disabled");
-                $('html, body').animate({
-                    scrollTop: $("#AND-gate-svg").offset().top
-                }, 500);
-
-                /* The first simulation iteration call is done immediately and then later iterations are called in intervals */
-                simulateANDGate(iterationNo, inputXofAND.join(","), inputYofAND.join(","), parseFloat(AND_w1), parseFloat(AND_w2), parseFloat(AND_threshold), interval);
-                iterationNo++;
-
-                var timer = window.setInterval(function () {
-                    simulateANDGate(iterationNo, inputXofAND.join(","), inputYofAND.join(","), parseFloat(AND_w1), parseFloat(AND_w2), parseFloat(AND_threshold), interval, timer);
-                    iterationNo++;
-                    if (iterationNo == 4) {
-                        window.clearInterval(timer);
-                    }
-                }, interval * 17);
-
-                timer4=window.setTimeout(function () {
-                    resetSimulationPart();                    
-                    $(".sliders").slider("enable");
-                    $("#startSimButton").removeClass("disabled");
-                    $("#selGate").prop("disabled", false);
-                    $("#stopSimButton").addClass("disabled");
-                    alert("Simulation Complete!");
-                }, interval * 17 * 4);
-
-
-                $("#stopSimButton").click(function () {
-                    stopSimulation(timer);
-                });
-
-            }
-
-            /* 
-            This code is making webpage unresponsives
-            function verifyANDOutputs() {
-            var correctANDOPs = [0, 0, 0, 1];
-            if (correctANDOPs.length == AND_calcOP.length) {
-            for (var i = 0; AND_calcOP.length; i++) {
-            if (correctANDOPs[i] != AND_calcOP[i]) {
-            alert("Incorrect Output values found!!! This threshold value does not work for this neural network.");
-            return;
-            }
-            }
-            alert("Correct Output values found! This threshold value works for this neural network.");
-            }
-            }
-            */
-        </script>
         <link href="../src/StyleSheet1.css" rel="stylesheet" />
+
+
+        <!--Here is the main script that handles the simulation-->
+        <script type="text/javascript" src="exp1Simulation.js"></script>    
+        <!--Here is the main CSS file that adds more touch to the simulation and other stuff-->    
         <link href="../Styles.css" rel="stylesheet" />
+
+
         <!-- Simulation scripts end-->
     </head>
     <body class="hold-transition skin-blue sidebar-mini">
@@ -326,8 +112,8 @@
                             <option value="NOT">NOT Gate</option>
                         </select>
                     </div>
-                    <button id="startSimButton" class="btn btn-success" onclick="startSimulation(1000)">Start Simulation</button>
-                    <button id="stopSimButton" class="btn btn-danger disabled">Stop Simulation</button><br/>
+                    <button id="startSimButton" class="btn btn-success" onclick="startANDSimulation(1000)">Start Simulation</button>
+                    <button id="stopSimButton" class="btn btn-danger disabled" onclick="stopANDSimulation()" disabled>Stop Simulation</button><br/>
                     <div>
                         <p id="ux-wrapper">y(x) = 1 , u(x) >= Threshold<br/>
                             &emsp;&emsp;= 0 , u(x) < Threshold</p>
@@ -335,13 +121,14 @@
                     <br/>
                     <div id="AND-gate-sim">
                             <b>w<sub>1</sub> : </b>
-                            <div id="AND_Gate_w1_slider" class="sliders" style="width: 200px;display: inline-block;"></div><br/>
+                            <div id="AND_Gate_w1_slider" class="sliders" style="width: 200px;display: inline-block;"></div>&emsp;<span class="AND-inputX-oplay_neuron1-weight"></span><br/>
 
                             <b>w<sub>2</sub> : </b>
-                            <div id="AND_Gate_w2_slider" class="sliders" style="width: 200px;display: inline-block;"></div><br/>
+                            <div id="AND_Gate_w2_slider" class="sliders" style="width: 200px;display: inline-block;"></div>&emsp;<span class="AND-inputY-oplay_neuron1-weight"></span><br/>
 
-                            <b>Threshold Value : </b>
-                            <div id="AND_Gate_Threshold_slider" class="sliders" style="width: 200px;display: inline-block;"></div><br/>
+                            <b>Threshold : </b>
+                            <div id="AND_Gate_Threshold_slider" class="sliders" style="width: 200px;display: inline-block;"></div>&emsp;<span class="AND-threshold-value"></span><br/>
+
                             <svg id="AND-gate-svg" width="700" height="300" style="float: left;">
                                 <!--Neural Network connections-->
                                 <line id="AND-inputX-oplay_neuron1" class="StdLine" x1="50" y1="50" x2="250" y2="150" style=""/>
@@ -374,12 +161,12 @@
                                 <!--y(x) related texts-->
                                 <text font-size="20" x="535" y="150">y(x)</text>
                                 <text class="changingTextStyle" id="AND-yx-value" font-size="20" x="535" y="170"></text>
-                                <text class="" id="AND-threshold-value" font-size="20" x="455" y="220">Threshold: 0</text>
+                                <text class="" font-size="20" x="455" y="220">Threshold: <tspan class="AND-threshold-value" ></tspan></text>
 
                                 <text class="changingTextStyle" id="AND-yx-value-expln" font-size="20" x="415" y="70"></text>
                             </svg>
                             <div style="font-family: 'Source Sans Pro', sans-serif;font-size: 20px;">                                
-                                <!--<button id="ANDNextButton" onclick="startSimulation(1000,0)" disabled>Move to next set of values</button><br/><br/>-->
+                                <button class="btn btn-warning disabled" id="ANDNextButton" disabled>Apply next set of I/P values</button><br/><br/>
                                 <h3>Truth Table of AND Gate</h3>
                                 <table class="table-condensed truthTable" style="">
                                     <tr><th>X</th><th>Y</th><th>Expected O/P</th><th>O/P from NN</th></tr>
