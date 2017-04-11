6af33a1868cb0665ef5aedcd50999df29b92516c
Varun Rao <varun21exps@gmail.com>
2017-04-11 22:34:58 +0530

    Trial Commit

--- VLabs Hosted Content/labs/exp2/simulation.php
+++ VLabs Hosted Content/labs/exp2/simulation.php
@@ -6,7 +6,7 @@ $_SESSION["currPage"]=5;
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title> Virtual Labs </title>
+  <title> Virtual Lab </title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
