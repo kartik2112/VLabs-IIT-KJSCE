6de0c6fdac54984a8eb4033daec683fcd01f8736
Kartik Shenoy <kartik.shenoyy@gmail.com>
2017-05-31 09:06:11 +0530

    Template added

---
+++ VLabs Hosted Content/labs/exp_template/lab_name.php
@@ -0,0 +1,8 @@
+<?php 
+$lab_name = "Machine Learning Lab";
+
+$exp_name = "Implementation of AND/OR/NOT Gates using Single Layer Perceptron";
+
+$_SESSION['lab_name'] = $lab_name;
+$_SESSION['exp_name'] = $exp_name;
+?>
