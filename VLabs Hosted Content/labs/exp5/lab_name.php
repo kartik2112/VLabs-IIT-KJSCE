7cfb9ca24bf5851b6d857bac10e834dc78ddf74f
Kartik Shenoy <kartik.shenoyy@gmail.com>
2017-06-06 13:06:12 +0530

    Lab Names changed

--- VLabs Hosted Content/labs/exp5/lab_name.php
+++ VLabs Hosted Content/labs/exp5/lab_name.php
@@ -1,7 +1,7 @@
 <?php 
 $lab_name = "Machine Learning Lab";
 
-$exp_name = "Learning Rules - Perceptron and Hebbian";
+$exp_name = "Hebbian Learning Rule";
 
 $_SESSION['lab_name'] = $lab_name;
 $_SESSION['exp_name'] = $exp_name;
