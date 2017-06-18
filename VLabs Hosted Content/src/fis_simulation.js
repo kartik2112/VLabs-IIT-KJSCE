5a7e17dace5418cd89567a403cd45f50d1215c69
Tejas <tejas.dastane@gmail.com>
2017-06-18 22:25:32 +0530

    Added expt FIS
    Made changes to Lab & Expt name. Also template for simulation kept
    ready.

---
+++ VLabs Hosted Content/src/fis_simulation.js
@@ -0,0 +1,11 @@
+var n_descriptor_qty = 3;
+var n_descriptor_dirt = 3;
+
+function add_descriptor(who){
+    // Add a descriptor for Quantity.
+    if(who==1){
+        n_descriptor_qty++;
+        var html_content_to_be_added = '<div class="descr" id="q_d_'+n_descriptor_qty+'"><input type="number" max="100" min="0" value="67"/><input type="text" placeholder="Name of descriptor"/><input type="number" max="100" min="0" value="100"/><button id="q'+n_descriptor_qty+'"><b>-</b></button></div>';
+        document.getElementById('descrs_1').innerHTML += html_content_to_be_added;
+    }
+}
