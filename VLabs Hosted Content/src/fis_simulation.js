5da81f0e88177751b25a4c32be6590cb17aea7bb
Tejas <tejas.dastane@gmail.com>
2017-06-18 22:28:30 +0530

    Small changes

--- VLabs Hosted Content/src/fis_simulation.js
+++ VLabs Hosted Content/src/fis_simulation.js
@@ -5,7 +5,7 @@ function add_descriptor(who){
     // Add a descriptor for Quantity.
     if(who==1){
         n_descriptor_qty++;
-        var html_content_to_be_added = '<div class="descr" id="q_d_'+n_descriptor_qty+'"><input type="number" max="100" min="0" value="67"/><input type="text" placeholder="Name of descriptor"/><input type="number" max="100" min="0" value="100"/><button id="q'+n_descriptor_qty+'"><b>-</b></button></div>';
+        var html_content_to_be_added = '<div class="descr" id="q_d_'+n_descriptor_qty+'"><input type="number" max="100" min="0" value="0"/><input type="text" placeholder="Name of descriptor"/><input type="number" max="100" min="0" value="100"/><button id="q'+n_descriptor_qty+'"><b>-</b></button></div>';
         document.getElementById('descrs_1').innerHTML += html_content_to_be_added;
     }
 }
