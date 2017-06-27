68726bd2f1a1cf8aa5f7c9d773819e9bafc8fa8c
Varun Rao <varun21exps@gmail.com>
2017-06-27 20:31:53 +0530

    Bug patch

--- VLabs Hosted Content/labs/expFis/posttest.php
+++ VLabs Hosted Content/labs/expFis/posttest.php
@@ -4,19 +4,24 @@
 ?>
 <html>
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title> Virtual Labs </title>
-        <!-- Tell the browser to be responsive to screen width -->
-        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <!-- Bootstrap 3.3.6 -->
-        <link rel="stylesheet" href="../../bootstrap/css/bootstrap.css">
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-        <!-- Theme style -->
-        <link rel="stylesheet" href="../../dist/css/AdminLTE.css">
-        <!-- AdminLTE Skins. Choose a skin from the css/skins folder instead of downloading all of them to reduce the load. -->
-        <link rel="stylesheet" href="../../dist/css/skins/_all-skins.min.css">
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title> Virtual Labs </title>
+      <!-- Tell the browser to be responsive to screen width -->
+      <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+      <!-- Bootstrap 3.3.6 -->
+      <link rel="stylesheet" href="../../bootstrap/css/bootstrap.css">
+      <!-- Font Awesome -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+      <!-- Theme style -->
+      <link rel="stylesheet" href="../../dist/css/AdminLTE.css">
+      <!-- AdminLTE Skins. Choose a skin from the css/skins folder instead of downloading all of them to reduce the load. -->
+      <link rel="stylesheet" href="../../dist/css/skins/_all-skins.min.css">
+
+
+      <!-- jQuery 2.2.3 -->
+      <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+      <link href="../../src/Styles.css" rel="stylesheet" />
         <script>
             $(document).ready(function(){
                 $(".optradio1").click(function(){
